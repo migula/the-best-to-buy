785f237f32bbf3b137151b40e3eabe4f9d4ba3db
migula <cool187@126.com>
2019-01-27 16:38:51 +0800

    楼层数据渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -22,7 +22,21 @@
         <p>{{item.name}}</p>
       </div>
     </div>
-    <!--楼层 -->
+    
+     <!-- 楼层区域 -->
+    <div class="floor-container">
+      <div class="floor" v-for="item in floorList">
+        <!-- 顶部 -->
+        <div class="top">
+          <img :src="item.floor_title.image_src" alt>
+          <h3>{{item.floor_title.name}}</h3>
+        </div>
+        <!-- 底部 -->
+        <div class="bottom">
+          <img v-for="(it,i)  in item.product_list" :src="it.image_src" alt>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,7 +46,9 @@ export default {
   data() {
     return {
       swiperList: [],
-      categoryList: []
+      categoryList: [],
+      floorList:[],
+
     };
   },
   async created() {
@@ -45,6 +61,13 @@ export default {
       url: "api/public/v1/home/catitems"
     });
     this.categoryList = navigateRes.data.message;
+      // 获取楼层数据
+    let floorRes = await hxios.get({
+      url: "api/public/v1/home/floordata"
+    });
+    console.log(floorRes);
+    this.floorList = floorRes.data.message;
+
   }
 };
 </script>
@@ -105,6 +128,56 @@ $ured: #ff2d4a;
       text-align: center;
       font-size: 24rpx;
       margin-top: 10rpx;
+    }
+  }
+}
+// 楼层数据
+.floor-container {
+  .floor {
+    .top {
+      padding-top: 30rpx;
+      padding-bottom: 30rpx;
+      padding-left: 15rpx;
+      position: relative;
+      height: 90rpx;
+      box-sizing: border-box;
+      h3 {
+        color: #ff7b94;
+        position: absolute;
+        left: 30rpx;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      img {
+        position: absolute;
+        height: 90rpx;
+        left: 0;
+        top: 0;
+        display: block;
+        width: 100%;
+      }
+    }
+    .bottom {
+      padding: 20rpx 0 0 16rpx;
+      overflow: hidden;
+      img {
+        display: block;
+        width: 33.333%;
+        float: left;
+        width: 230rpx;
+        height: 190rpx;
+        margin-right: 10rpx;
+        &:first-child {
+          width: 230rpx;
+          height: 390rpx;
+        }
+        &:nth-child(2) {
+          margin-bottom: 10rpx;
+        }
+        &:nth-child(3) {
+          margin-bottom: 10rpx;
+        }
+      }
     }
   }
 }
