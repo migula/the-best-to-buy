698ddbdb2fa3bbe14d4588e34e43e1493ced90e6
migula <migula@cool187@126.com>
2019-02-01 08:31:16 +0800

    分类完成"

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,46 +1,61 @@
 <template>
   <div class="category-container">
-    <!-- 抽取的组件名 -->
-    <searchBox></searchBox>
+    <!-- 使用组件 -->
+    <searchbox></searchbox>
+    <!-- 滚动的区域 -->
     <div class="scroll-box">
-      <!-- 左边盒子 -->
       <scroll-view class="left" scroll-y scroll-with-animation>
-        <!-- 渲染数据 -->
         <ul>
-            <!-- @click="index==i" 点击高亮-->
           <li
-            :class="{actived:index===i}"
             v-for="(item, i) in categoryList"
+            :class="{actived:index===i}"
             :key="item.cat_id"
             @click="index=i"
           >{{item.cat_name}}</li>
         </ul>
       </scroll-view>
-      <!-- 右边盒子 -->
-      <scroll-view class="right" scroll-y scroll-with-animation></scroll-view>
+      <scroll-view class="right" scroll-y scroll-with-animation>
+        <img class="title-img" src="/static/icon/titleImage.png" alt>
+        <block v-if="categoryList.length!=0">
+          <div class="section" v-for="level1 in categoryList[index].children" :key="level1.cat_id">
+            <div class="title">
+              <span>/</span>
+              &nbsp;&nbsp;&nbsp;&nbsp;{{level1.cat_name}}&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>/</span>
+            </div>
+            <div class="items">
+              <div class="item" v-for="(level2,i) in level1.children" :key="level2.cat_id">
+                <img :src="'https://autumnfish.cn/wx/'+level2.cat_icon" alt>
+                <p>{{level2.cat_name}}</p>
+              </div>
+            </div>
+          </div>
+        </block>
+      </scroll-view>
     </div>
   </div>
 </template>
 
 <script>
-// 导入抽取的组件
-import searchBox from "../../components/searchBox";
+// 导入组件
+import searchbox from "../../components/searchBox.vue";
 // 导入hxios
 import hxios from "../../utils/index.js";
-
 export default {
   data() {
     return {
+      // 分类数据
       categoryList: [],
-      index:0
+      // 索引值
+      index: 0
     };
   },
   // 注册组件
-  components:{
-    searchBox
+  components: {
+    searchbox
   },
-  // 获取数据
-  async created() {
+  // async created() {
+  async onLoad() {
     let res = await hxios.get({
       url: "api/public/v1/categories"
     });
@@ -50,10 +65,15 @@ export default {
 };
 </script>
 
-<style lang='scss'>
-$uRed:#ff2d4d;
-page{
+<style lang="scss">
+$uRed: #ff2d4a;
+page {
   height: 100%;
+}
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+  color: transparent;
 }
 .category-container {
   padding-top: 100rpx;
@@ -95,7 +115,43 @@ page{
     }
     .right {
       flex: 1;
-      background-color: skyblue;
+      // background-color: skyblue;
+      padding: 15rpx;
+      box-sizing: border-box;
+      .title-img {
+        display: block;
+        width: 100%;
+        height: 200rpx;
+      }
+      .section {
+        margin-top: 40rpx;
+        .title {
+          text-align: center;
+          font-size: 28rpx;
+          span {
+            color: #ccc;
+          }
+        }
+        .items {
+          display: flex;
+          flex-wrap: wrap;
+          margin-top: 20rpx;
+          .item {
+            width: 33.3333%;
+            img {
+              display: block;
+              width: 100rpx;
+              height: 70rpx;
+              margin: 0 auto;
+            }
+            p {
+              text-align: center;
+              margin-top: 20rpx;
+              font-size: 26rpx;
+            }
+          }
+        }
+      }
     }
   }
 }
